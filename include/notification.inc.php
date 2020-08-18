71a1f12a3d8aa436a1582f715bf3774d8cffa0f5
mambax7 <mambax7@gmail.com>
2020-08-18 15:58:47 -0400

    reverse of double quotes

--- include/notification.inc.php
+++ include/notification.inc.php
@@ -24,8 +24,33 @@ if (!preg_match('/^Myalbum\d*$/', $moduleDirName)) {
     exit('invalid dirname of myalbum: ' . htmlspecialchars($moduleDirName, ENT_QUOTES | ENT_HTML5));
 }
 
-eval('function ' . $moduleDirName . ' . $moduleDirName . ');
-        $module = $moduleHandler->getByDirname("' . $moduleDirName . '.$xoopsDB->prefix("\'' . $moduleDirName . ' . $moduleDirName . '";
+eval('function ' . $moduleDirName . '_notify_iteminfo($not_category, $item_id){
+    global $xoopsModule, $xoopsModuleConfig, $xoopsConfig , $xoopsDB ;
+
+    if (empty($xoopsModule) || $xoopsModule->getVar("dirname") != "' . $moduleDirName . '" ) {
+        $moduleHandler = xoops_getHandler("module");
+        $module = $moduleHandler->getByDirname("' . $moduleDirName . '");
+        $configHandler = xoops_getHandler("config");
+        $config = $configHandler->getConfigsByCat(0,$module->getVar("mid"));
+    } else {
+        $module =& $xoopsModule;
+        $config = $xoopsModuleConfig;
+    }
+    $mod_url = XOOPS_URL . "/modules/" . $module->getVar("dirname") ;
+
+    if ($not_category=="global") {
+        $item["name"] = "";
+        $item["url"] = "";
+    } elseif ($not_category == "category") {
+        // Assume we have a valid cid
+        $sql = "SELECT title FROM ".$xoopsDB->prefix("' . $moduleDirName . '";
+        $rs = $xoopsDB->query( $sql ) ;
+        list( $title ) = $xoopsDB->fetchRow( $rs ) ;
+        $item[\'name\'] = $title ;
+        $item[\'url\'] = "$mod_url/viewcat.php?cid=$item_id" ;
+    } elseif ($not_category == "photo") {
+        // Assume we have a valid event_id
+        $sql = \'SELECT title FROM \'.$xoopsDB->prefix(\'' . $moduleDirName . '";
         $rs = $xoopsDB->query( $sql ) ;
         list( $title ) = $xoopsDB->fetchRow( $rs ) ;
         $item[\'name\'] = $title ;
